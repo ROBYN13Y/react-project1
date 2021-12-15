f8554a79d57e9c4fdbc5f3502d2f3d155752fd4e
ROBYN13Y <yrobyn3@gmail.com>
2021-12-15 14:57:38 -0800

    UPDATE
    header
    footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>SheCodes React Project 1</p>
       </header>
+
+      <footer className="App-footer">
+        <p>
+          <span className="Footer-emoji">🔗</span>{" "}
+          <a
+            className="App-link"
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-sourced
+          </a>{" "}
+          Code |{" "}
+          <a
+            className="App-link"
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Robyn Young
+          </a>{" "}
+          <span className="Footer-emoji">👩‍💻</span>
+        </p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+></a>;
